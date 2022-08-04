3d023874d97c77510d2630627d6295c4f1de1f9c
blinmyach <politaewnazar@gmail.com>
2022-08-05 00:53:45 +0300

    Bulet particle
    партикли крові і іск

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
 
+    public GameObject hitBlood;
     public GameObject hitEffect;
     public int damage = 10;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1.5f);
+       
+        Vector2 dir = GetComponent<Rigidbody2D>().velocity.normalized;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        
+        if (collision.CompareTag("AI") || collision.CompareTag("Hero_Player"))
+        {
+            GameObject effect = Instantiate(hitBlood, transform.position, Quaternion.Euler(0, 0, angle));
+        }
+        else
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.Euler(0, 0, angle));
+        }
 
         Unit unit = collision.gameObject.GetComponent<Unit>();
 
