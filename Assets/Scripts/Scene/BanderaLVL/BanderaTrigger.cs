b481b6e61fc6df0395c8ebdf93a0c29b0768fabf
GomonMax <gomonm3@gmail.com>
2022-08-25 18:41:34 +0300

    New micro details
    Добавлено кілька речей, які покращують гру (стрілки в кінці рівнів(куда йти), лампи мигають на 3 рівні і добавлені гарні переходи на нові рівні)
    
    Co-Authored-By: Vladislav Bondar <108332329+Skvadik19@users.noreply.github.com>
    Co-Authored-By: blinmyach <69404038+blinmyach@users.noreply.github.com>

--- Assets/Scripts/Scene/BanderaLVL/BanderaTrigger.cs
+++ Assets/Scripts/Scene/BanderaLVL/BanderaTrigger.cs
@@ -6,21 +6,38 @@ using UnityEngine.UI;
 public class BanderaTrigger : MonoBehaviour
 {
     public GameObject[] triggerSpawn;
+    public GameObject[] lamp;
+
     public BanderaMove bandera;
     public GameObject slider;
+
+    public bool allow;
+
+    void Update()
+    {
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Hero_Player"))
         {
             slider.SetActive(true);
+            bandera.Move();
+            allow = true;
+
             for(int i = 0; i < triggerSpawn.Length; i++)
             {
                 triggerSpawn[i].SetActive(true);
-                bandera.Move();
-
-                //bandera.Patrol();
+                LampTrue();
             }
+        }
+    }
+
+    void LampTrue()
+    {
+        for(int i = 0; i < lamp.Length; i++)
+        {
+            lamp[i].SetActive(true);
         }
     }
 }
--- Assets/Scripts/Scene/LockDoor.cs
+++ Assets/Scripts/Scene/LockDoor.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class LockDoor : MonoBehaviour
 {
+    public GameObject arrow;
     void Update()
     {
         if(!GameObject.FindWithTag("AI"))
         {
             gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            arrow.SetActive(true);
         }
         else
         gameObject.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
