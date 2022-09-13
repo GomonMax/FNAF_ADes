f10ce4b846d45b880886b70e647867026c78a804
GomonMax <gomonm3@gmail.com>
2022-09-13 13:17:42 +0300

    fix

--- Assets/Scripts/Scene/EndScreenLoad.cs
+++ Assets/Scripts/Scene/EndScreenLoad.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndScreenLoad : MonoBehaviour
 {
     public int level;
+    public TextMeshProUGUI completeText;
+
+    void Start()
+    {
+        level = PlayerPrefs.GetInt("lvlLOAD");
+        completeText.text = "Level " + level.ToString() + " Complete";
+    }
 
     void Update()
     {
         if(Input.anyKeyDown)
         {
-            level = PlayerPrefs.GetInt("lvlLOAD");
             level +=1;
             SceneManager.LoadScene(level);
         }
