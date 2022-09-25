99cf705469387de04dd4a6725c699eb61acc81cd
GomonMax <gomonm3@gmail.com>
2022-09-25 19:25:37 +0300

    SOUND

--- Assets/Scripts/Scene/BanderaLVL/EndLVL.cs
+++ Assets/Scripts/Scene/BanderaLVL/EndLVL.cs
@@ -11,14 +11,22 @@ public class EndLVL : MonoBehaviour
     public GameObject[] spawn;
     public GameObject arrow;
     public GameObject block;
+    public GameObject boom;
+    //private AudioSource audio;
+    //public AudioClip boom;
+
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Bandera"))
         {
+            //audio = GetComponent<AudioSource>();
             nextlvl.SetActive(true);
             arrow.SetActive(true);
             block.SetActive(false);
+            //audio.PlayOneShot(boom);
+            boom.SetActive(true);
             bandera.hp = 999999999;
             bandera.pipsi = false;
             wall.SetActive(true);         
